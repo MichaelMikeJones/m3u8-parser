260d58fb2263c290b9c94eb04e05237196206aec
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2024-04-21 20:11:36 +0330

    fix(writer): fix lodash import

--- src/writer.js
+++ src/writer.js
@@ -1,8 +1,8 @@
-import isEqual from 'lodash.isequal';
+import isEqual from "lodash/isEqual";
 
-const handleMediaGroups = function(obj) {
+const handleMediaGroups = function (obj) {
   const keys = Object.keys(obj);
-  let result = '';
+  let result = "";
 
   keys.forEach((key) => {
     // samples
@@ -31,32 +31,32 @@ const handleMediaGroups = function(obj) {
           const value = groupData[Name][entryKey];
 
           const QuotedAttributes = [
-            'URI',
-            'LANGUAGE',
-            'ASSOC-LANGUAGE',
-            'INSTREAM-ID',
-            'CHARACTERISTICS',
-            'CHANNELS'
+            "URI",
+            "LANGUAGE",
+            "ASSOC-LANGUAGE",
+            "INSTREAM-ID",
+            "CHARACTERISTICS",
+            "CHANNELS",
           ];
 
-          if (entryKey === 'instreamId') {
+          if (entryKey === "instreamId") {
             // special case: key not compatible with rfc
-            entryKey = 'INSTREAM-ID';
+            entryKey = "INSTREAM-ID";
           }
 
           const isQuoted = QuotedAttributes.includes(entryKey.toUpperCase());
 
-          mediaGroupItem += `,${entryKey.toUpperCase()}=${isQuoted ? '"' : ''}${value}${isQuoted ? '"' : ''}`;
-
+          mediaGroupItem += `,${entryKey.toUpperCase()}=${
+            isQuoted ? '"' : ""
+          }${value}${isQuoted ? '"' : ""}`;
         });
 
-        result += mediaGroupItem + '\n';
+        result += mediaGroupItem + "\n";
       });
     });
-
   });
 
-  return result + '\n';
+  return result + "\n";
 };
 
 function handlePlaylists(arrPlaylists) {
@@ -64,48 +64,47 @@ function handlePlaylists(arrPlaylists) {
   // #EXT-X-STREAM-INF:AVERAGE-BANDWIDTH=20985770,BANDWIDTH=28058971,VIDEO-RANGE=SDR,CODECS="hvc1.2.4.L150.B0",RESOLUTION=3840x2160,FRAME-RATE=23.976,CLOSED-CAPTIONS=NONE,HDCP-LEVEL=TYPE-1
   // sdr_2160/prog_index.m3u8
 
-  let result = '';
+  let result = "";
 
   arrPlaylists.forEach((playlist) => {
-
-    let playlistItem = '#EXT-X-STREAM-INF:';
+    let playlistItem = "#EXT-X-STREAM-INF:";
 
     const attrKeys = Object.keys(playlist.attributes);
 
     attrKeys.forEach((attribute) => {
-
       // FIXME: an enumarated-string NONE should not be quoted in CLOSED-CAPTION attr
 
       const QuotedAttributes = [
-        'CODECS',
-        'AUDIO',
-        'VIDEO',
-        'SUBTITLES',
-        'CLOSED-CAPTIONS'
+        "CODECS",
+        "AUDIO",
+        "VIDEO",
+        "SUBTITLES",
+        "CLOSED-CAPTIONS",
       ];
 
       const isQuoted = QuotedAttributes.includes(attribute.toUpperCase());
 
       let value = playlist.attributes[attribute];
 
-      if (attribute === 'RESOLUTION') {
+      if (attribute === "RESOLUTION") {
         // changing object data to hls desired format
         value = `${value.width}x${value.height}`;
       }
 
-      if (attribute === 'FRAME-RATE') {
+      if (attribute === "FRAME-RATE") {
         // rounding FRAME-RATE to 3 decimal places
         value = parseFloat(value).toFixed(3);
       }
 
-      playlistItem += `,${attribute.toUpperCase()}=${isQuoted ? '"' : ''}${value}${isQuoted ? '"' : ''}`;
-
+      playlistItem += `,${attribute.toUpperCase()}=${
+        isQuoted ? '"' : ""
+      }${value}${isQuoted ? '"' : ""}`;
     });
 
     // adding uri
     playlistItem += `\n${playlist.uri}\n`;
 
-    result += playlistItem + '\n';
+    result += playlistItem + "\n";
   });
 
   return result;
@@ -113,20 +112,18 @@ function handlePlaylists(arrPlaylists) {
 
 function toHexString(uint32) {
   return uint32.reduce(
-    (output, elem) =>
-      (output + ('00000000' + elem.toString(16)).slice(-8)),
-    ''
+    (output, elem) => output + ("00000000" + elem.toString(16)).slice(-8),
+    ""
   );
 }
 
 function handleSegments(segments) {
-
-  let result = '';
+  let result = "";
   let lastKey = {};
   let lastMap = {};
 
   segments.forEach((segment) => {
-    let segmentString = '\n';
+    let segmentString = "\n";
 
     // if segment.key does not exists, it does not mean that no tag should be placed there
     // it just mean there is no key associated with that segment
@@ -142,24 +139,26 @@ function handleSegments(segments) {
     // add object equality function. They are confusing and ugly
     if (segment.key && !isEqual(segment.key, lastKey)) {
       // segment key exists and is not equel to last key
-      const IV = segment.key.iv ? `,IV=0x${toHexString(segment.key.iv)}` : '';
+      const IV = segment.key.iv ? `,IV=0x${toHexString(segment.key.iv)}` : "";
 
       segmentString += `#EXT-X-KEY:METHOD=${segment.key.method},URI="${segment.key.uri}"${IV}\n`;
 
       lastKey = segment.key;
     } else if (!segment.key && !isEmpty(lastKey)) {
       // segment.key does not exists and if we have a lastkey, we sould add #EXT-X-KEY:METHOD=NONE
-      segmentString += '#EXT-X-KEY:METHOD=NONE\n';
+      segmentString += "#EXT-X-KEY:METHOD=NONE\n";
     }
 
     if (segment.map && !isEqual(segment.map, lastMap)) {
       // segment map exists and is not equel to last map
 
-      if (typeof segment.map.byterange.offset === 'undefined') {
+      if (typeof segment.map.byterange.offset === "undefined") {
         segment.map.byterange.offset = 0;
       }
 
-      const BYTERANGE = segment.map.byterange ? `,BYTERANGE="${segment.map.byterange.length}@${segment.map.byterange.offset}"` : '';
+      const BYTERANGE = segment.map.byterange
+        ? `,BYTERANGE="${segment.map.byterange.length}@${segment.map.byterange.offset}"`
+        : "";
 
       segmentString += `#EXT-X-MAP:URI="${segment.map.uri}"${BYTERANGE}\n`;
 
@@ -167,7 +166,7 @@ function handleSegments(segments) {
     }
 
     if (segment.discontinuity) {
-      segmentString += '#EXT-X-DISCONTINUITY\n';
+      segmentString += "#EXT-X-DISCONTINUITY\n";
     }
 
     /*
@@ -197,11 +196,10 @@ function handleSegments(segments) {
 
     segmentString += segment.uri;
 
-    result += segmentString + '\n';
+    result += segmentString + "\n";
   });
 
   return result;
-
 }
 
 function stringifyTag(key, value) {
@@ -209,51 +207,54 @@ function stringifyTag(key, value) {
 
   // TODO: should we include #EXT-X-PROGRAM-DATE-TIME as playlist tag?
 
-  if (key === 'allowCache') {
+  if (key === "allowCache") {
     // FIXME: this tag is deprecated
-    return `#EXT-X-ALLOW-CACHE:${value ? 'YES' : 'NO'}\n`;
-  } else if (key === 'discontinuityStarts') {
+    return `#EXT-X-ALLOW-CACHE:${value ? "YES" : "NO"}\n`;
+  } else if (key === "discontinuityStarts") {
     // not implemented
-    return '';
-  } else if (key === 'version') {
+    return "";
+  } else if (key === "version") {
     return `#EXT-X-VERSION:${value}\n`;
-  } else if (key === 'mediaGroups') {
+  } else if (key === "mediaGroups") {
     // handling media groups seperately
     return handleMediaGroups(value);
-  } else if (key === 'playlists') {
+  } else if (key === "playlists") {
     // handling media groups seperately
     return handlePlaylists(value);
-  } else if (key === 'targetDuration') {
+  } else if (key === "targetDuration") {
     return `#EXT-X-TARGETDURATION:${value}\n`;
-  } else if (key === 'mediaSequence') {
+  } else if (key === "mediaSequence") {
     return `#EXT-X-MEDIA-SEQUENCE:${value}\n`;
-  } else if (key === 'playlistType') {
+  } else if (key === "playlistType") {
     return `#EXT-X-PLAYLIST-TYPE:${value}\n`;
-  } else if (key === 'segments') {
+  } else if (key === "segments") {
     // handle segments seperately
     return handleSegments(value);
-  } else if (key === 'discontinuitySequence') {
+  } else if (key === "discontinuitySequence") {
     return `#EXT-X-DISCONTINUITY-SEQUENCE:${value}\n`;
-  } else if (key === 'start') {
+  } else if (key === "start") {
     return `#EXT-X-START:TIME-OFFSET=${value.timeOffset},PRECISE=${value.precise}\n`;
-  } else if (key === 'endList') {
+  } else if (key === "endList") {
     if (value) {
-      return '#EXT-X-ENDLIST\n';
+      return "#EXT-X-ENDLIST\n";
     }
 
     // if value is false tag should not be present
-    return '';
-  } else if (key === 'independentSegments') {
-    return '#EXT-X-INDEPENDENT-SEGMENTS\n';
+    return "";
+  } else if (key === "independentSegments") {
+    return "#EXT-X-INDEPENDENT-SEGMENTS\n";
   }
 
   // unknown tag
-  return '';
-
+  return "";
 }
 
 function isEmpty(element) {
-  if (typeof element === 'object' && (element.constructor === Object || element.constructor === Array) && Object.keys(element).length === 0) {
+  if (
+    typeof element === "object" &&
+    (element.constructor === Object || element.constructor === Array) &&
+    Object.keys(element).length === 0
+  ) {
     // typeof [] === 'object'
     // Object.keys(["One", "Two", "Three"]) === ["0", "1", "2"]
     return true;
@@ -266,15 +267,26 @@ function isEmpty(element) {
 // ["EXT-X-VERSION", "#EXT-X-MEDIA", "#EXT-X-STREAM-INF", "#EXT-X-TARGETDURATION", "#EXT-X-MEDIA-SEQUENCE", "#EXT-X-PLAYLIST-TYPE", "#EXT-X-DISCONTINUITY-SEQUENCE", "#EXT-X-INDEPENDENT-SEGMENTS", "#EXT-X-START", "#EXTINF", "#EXT-X-ENDLIST"];
 // #EXT-X-PROGRAM-DATE-TIME , #EXT-X-KEY , #EXT-X-MAP , #EXT-X-BYTERANGE and #EXT-X-DISCONTINUITY will be created in segments
 // below is equivalent in manifest object
-const tagsInOrder = ['version', 'mediaGroups', 'playlists', 'targetDuration', 'mediaSequence', 'playlistType', 'discontinuitySequence', 'independentSegments', 'start', 'segments', 'endList'];
+const tagsInOrder = [
+  "version",
+  "mediaGroups",
+  "playlists",
+  "targetDuration",
+  "mediaSequence",
+  "playlistType",
+  "discontinuitySequence",
+  "independentSegments",
+  "start",
+  "segments",
+  "endList",
+];
 
 export default function Writer(manifest) {
-
-  let stringified = '';
+  let stringified = "";
 
   // adding #EXTM3U in the first place
   // adding new line manullay in every line
-  stringified += '#EXTM3U\n';
+  stringified += "#EXTM3U\n";
 
   const manifestKeys = Object.keys(manifest);
 
@@ -286,13 +298,12 @@ export default function Writer(manifest) {
   });
 
   // remove , after :
-  stringified = stringified.replace(/:,/g, ':');
+  stringified = stringified.replace(/:,/g, ":");
 
   // replacing true and false with YES and NO
   // because true and false were used in manifest obj
-  stringified = stringified.replace(/true/g, 'YES');
-  stringified = stringified.replace(/false/g, 'NO');
+  stringified = stringified.replace(/true/g, "YES");
+  stringified = stringified.replace(/false/g, "NO");
 
   return stringified;
-
 }
